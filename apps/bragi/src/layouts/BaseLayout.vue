317f0c28da0bbf36e5ddf73e12d048ea0d5bbc68
Yuval Levi <yuvall9313@gmail.com>
2022-06-04 02:32:18 +0300

    Add basic layout

---
+++ apps/bragi/src/layouts/BaseLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h-screen grid sm:grid-cols-main grid-rows-main">
+    <main class="sm:row-web">
+      <slot></slot>
+    </main>
+
+    <div class="hidden sm:block sm:row-web">
+      <slot name="socialBar"></slot>
+    </div>
+
+    <div class="col-span-full z-50">
+      <slot name="actionBar"></slot>
+    </div>
+
+    <div class="sm:order-first sm:row-web">
+      <slot name="sideBar"></slot>
+    </div>
+  </div>
+</template>
--- apps/bragi/tailwind.config.js
+++ apps/bragi/tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        main: '5fr minmax(100px, 9fr) 3fr',
+      },
+      gridTemplateRows: {
+        main: '8fr 1fr 1fr',
+      },
+      gridRow: {
+        web: 'span 2 / span 2',
+      }
+    },
   },
   plugins: [],
-}
+};
