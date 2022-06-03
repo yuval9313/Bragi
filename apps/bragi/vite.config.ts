ed50845ad1a3c0c294549ae2927f2362d3ead3f3
Yuval Levi <yuvall9313@gmail.com>
2022-06-03 18:20:38 +0300

    Apply new vue package

---
+++ apps/bragi/vite.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+const path = require('path');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve:{
+    alias:{
+      '@' : path.resolve(__dirname, './src')
+    },
+  },
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  }
+})
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,5 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  extends: ["plugin:vue/vue3-recommended", "prettier"],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
   },
 };
