a65a7e690e5c082b66372980b717e81aa8545f53
Yuval Levi <yuvall9313@gmail.com>
2022-06-03 19:10:51 +0300

    Fix all issues with imports and such

--- apps/bragi/vite.config.ts
+++ apps/bragi/vite.config.ts
@@ -1,16 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const path = require('path');
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src')
-    },
-  },
-  plugins: [vue()],
+  plugins: [tsconfigPaths(), vue()],
   test: {
     globals: true,
     environment: 'jsdom',
