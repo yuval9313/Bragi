5e52e8ae3e87d500ffa64a304db7f6ac123b533d
Yuval Levi <yuvall9313@gmail.com>
2022-06-05 16:59:31 +0300

    Compose side bar layout into library bar component

---
+++ apps/bragi/src/components/LibraryBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import SideBar from "@/layouts/SideBar.vue";
+import NavigationButtons from "@/components/NavigationButtons.vue";
+
+</script>
+
+<template>
+  <SideBar>
+    <template #helpMenu> Help Menu </template>
+    <template #navigationButtons>
+      <NavigationButtons />
+    </template>
+    <template #library> Library buttons </template>
+  </SideBar>
+</template>
