99cf9eef18dfc03fcd44c5125f54e6a495e0a96a
Yuval Levi <yuvall9313@gmail.com>
2022-06-05 16:59:08 +0300

    Add sideBar

---
+++ apps/bragi/src/layouts/SideBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="hidden sm:block">
+    <slot name="helpMenu"></slot>
+  </div>
+  <slot name="navigationButtons"></slot>
+  <div class="flex flex-col">
+    <slot name="library"></slot>
+  </div>
+</template>
